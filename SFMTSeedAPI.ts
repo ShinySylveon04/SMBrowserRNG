7f1400e65cd3fa3597405ac623ad1dceb0291629
EzPzStreamz <steviecook210@gmail.com>
2017-04-02 15:10:48 -0400

    Finished SFMTSeedAPI

--- SFMTSeedAPI.ts
+++ SFMTSeedAPI.ts
@@ -1,22 +1,20 @@
-﻿export class Result {
-    public seed: string;
-    public encoded_needle: string;
-    public step: number;
+﻿export interface Result {
+    seed: string;
+    encoded_needle: string;
+    step: number;
 }
 
-export class Root {
-    public results: Array<Result>;
+export interface Root {
+    results: Array<Result>;
 }
 
 export function request(needle: string) {
     var root: Root;
     var url = "http://49.212.217.137:19937/gen7/sfmt/seed?needle=" + needle;
 
-    var jsonStr: string;
-
     var Httpreq = new XMLHttpRequest();
     Httpreq.open("GET", url, false);
     Httpreq.send(null);
-    jsonStr = JSON.parse(Httpreq.responseText);
-
+    root = JSON.parse(Httpreq.responseText);
+    return root;
 }
