16de454ec7721186fdf00aacc14efb2c5031e571
Admiral-Fish <admiralfish420@gmail.com>
2017-04-01 22:14:40 -0700

    TinyMT/SMFT and Licenses

---
+++ TinyMTParameter.ts
@@ -0,0 +1,13 @@
+﻿export class TinyMTParameter
+{
+    public mat1: number;
+    public mat2: number;
+    public tmat: number;
+
+    constructor(mat1: number, mat2: number, tmat: number)
+    {
+        this.mat1 = mat1;
+        this.mat2 = mat2;
+        this.tmat = tmat;
+    }
+}
