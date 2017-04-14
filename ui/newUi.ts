ac6c308bce685da951618c859c392b31ae1ccd4d
Zaksabeast <zaksabeastdev@gmail.com>
2017-04-14 10:28:43 -0600

    egg rng is now fully linked with our current fields, and masterUi now has working tabs

--- ui/newUi.ts
+++ ui/newUi.ts
@@ -5,6 +5,7 @@ export default class masterUi {
         constructor(document: any){
 
             //Create Lists
+            let tabOjects = {};
             let listList = [
                 ["RNG Tools", "Guides", "Help", "About"],
                 ["Eggs", "Wild", "Wonder Card", "UB"],
@@ -39,16 +40,19 @@ export default class masterUi {
             donateUl.appendChild(donateLi);
 
             //Create Top Tabs
-            for(let j = 0; j<3; j++){
+            for(let j = 0; j<idList.length; j++){
                 let topTabs = document.createElement("ul");
                 topTabs.setAttribute("id", idList[j]);
                 for(let i = 0; i<listList[j].length; i++){
                     let listItem = document.createElement("li");
                     let linkItem = document.createElement("a");
                     linkItem.setAttribute("class", classList[j]);
+                    linkItem.setAttribute("id", listList[j][i].replace(/\s/g,'').toLowerCase());
                     linkItem.innerHTML = listList[j][i];
                     listItem.appendChild(linkItem);
+                    linkItem.addEventListener("click", ()=>createComingSoon(document, settingsContainer, viewContainer));
                     topTabs.appendChild(listItem);
+                    tabOjects[listList[j][i].replace(/\s/g,'').toLowerCase()] = linkItem;
                 }
                 document.body.appendChild(topTabs);
                 if(j==0){
@@ -56,15 +60,48 @@ export default class masterUi {
                 }
             }
 
+            tabOjects["eggs"].addEventListener("click", ()=>createGen7EggUi(document, settingsContainer, viewContainer));
+
             //Settings container
             let settingsContainer = document.createElement("div");
             settingsContainer.setAttribute("class", "settingsContainer");
             document.body.appendChild(settingsContainer);
 
+            //View Container
+            let viewContainer = document.createElement("div");
+            viewContainer.setAttribute("class", "settingsContainer");
+            document.body.appendChild(viewContainer);
+
             //Call the Gen 7 Egg UI by default
-            let newUi = new gen7EggUi(document, settingsContainer);
+            let newUi = new gen7EggUi(document, settingsContainer, viewContainer);
 
             //Add Donate Button Last
             document.body.appendChild(donateUl);
         }
+}
+
+function createComingSoon(document, settingsContainer, viewContainer){
+    //Remove Previous Containers
+    while (settingsContainer.firstChild) {
+        settingsContainer.removeChild(settingsContainer.firstChild);
+    }
+    while (viewContainer.firstChild) {
+        viewContainer.removeChild(viewContainer.firstChild);
+    }
+
+    var soon = document.createElement("h2");
+    soon.innerHTML = "This will be added soon :D";
+
+    settingsContainer.appendChild(soon);
+}
+
+function createGen7EggUi(document, settingsContainer, viewContainer){
+    //Remove Previous Containers
+    while (settingsContainer.firstChild) {
+        settingsContainer.removeChild(settingsContainer.firstChild);
+    }
+    while (viewContainer.firstChild) {
+        viewContainer.removeChild(viewContainer.firstChild);
+    }
+    new gen7EggUi(document, settingsContainer, viewContainer);
 }
